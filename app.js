d2438089aecacfa0f8dc4cb2c786a659c26c8abc
Эржигит Имамов <erjigit17@gmail.com>
2021-09-30 14:35:31 +0600

    add robot txt

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ http
         res.writeHead(200, { 'Content-Type': 'text/html' })
         res.end(html)
       })
+    } else if (req.url.match('.txt$')) {
+      var cssPath = path.join(__dirname, 'public', req.url)
+      var fileStream = fs.createReadStream(cssPath, 'UTF-8')
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      fileStream.pipe(res)
     } else if (req.url.match('.css$')) {
       var cssPath = path.join(__dirname, 'public', req.url)
       var fileStream = fs.createReadStream(cssPath, 'UTF-8')
